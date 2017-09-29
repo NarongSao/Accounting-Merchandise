6d889f05d56d8ba3d38888f11cc7b96d3b974ceb
Narong <narongsao98@gmail.com>
2017-09-29 09:02:06 +0700

    ensure index

--- acc/server/collection_caches/Journal.js
+++ acc/server/collection_caches/Journal.js
@@ -3,5 +3,6 @@ import 'meteor/theara:collection-cache';
 // Collection
 import {Journal} from '../../imports/api/collections/journal';
 
+Journal._ensureIndex({refId: 1, refFrom: 1, branchId: 1, voucherId: 1,journalDate: 1,total: 1}, {unique: 1, sparse: 1});
 
 Journal.cacheTimestamp();
